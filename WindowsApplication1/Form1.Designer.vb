e40d50a34abd1216f81780d13f97c2074b9623b3
Luis Felipe <31326261+Shadowli666@users.noreply.github.com>
2024-10-31 12:07:43 -0400

    Add files via upload

---
+++ WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,71 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.num1 = New System.Windows.Forms.TextBox()
+        Me.num2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(176, 82)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Tie"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'num1
+        '
+        Me.num1.Location = New System.Drawing.Point(85, 188)
+        Me.num1.Name = "num1"
+        Me.num1.Size = New System.Drawing.Size(100, 20)
+        Me.num1.TabIndex = 1
+        '
+        'num2
+        '
+        Me.num2.Location = New System.Drawing.Point(213, 188)
+        Me.num2.Name = "num2"
+        Me.num2.Size = New System.Drawing.Size(100, 20)
+        Me.num2.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(453, 270)
+        Me.Controls.Add(Me.num2)
+        Me.Controls.Add(Me.num1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents num1 As System.Windows.Forms.TextBox
+    Friend WithEvents num2 As System.Windows.Forms.TextBox
+
+End Class
---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show(Int(num1.Text) + Int(num2.Text))
+    End Sub
+
+End Class
